decd68a0a63800486c284d7023363d334f859602
Swephoenix <andreas.jansson.2010@gmail.com>
2026-04-18 17:21:01 +0200

    Add start.sh script

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Startar en Python-server för racing-spelet
+PORT=8000
+
+echo "Startar Koenigsegg Agera Racing Track på http://localhost:$PORT"
+
+# Försök öppna webbläsaren automatiskt
+if command -v xdg-open > /dev/null; then
+    xdg-open "http://localhost:$PORT" &
+elif command -v open > /dev/null; then
+    open "http://localhost:$PORT" &
+fi
+
+# Starta servern
+python3 -m http.server $PORT
